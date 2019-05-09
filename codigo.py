a9c97495877ddc423fdfddfccad1415beaa33910
unknown <luisfml@al.insper.edu.br>
2019-05-09 17:08:33 -0300

    Adicionei e está funcionando a imagem de fundo

--- codigo.py
+++ codigo.py
@@ -9,10 +9,52 @@ import time
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'imagens')
 
+# Dados gerais do jogo.
+WIDTH = 600 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
 
+# Definindo cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Carrega o fundo do jogo
-background = pygame.image.load(path.join(img_dir, '"imagem_fundo.jpg"')).convert()
+background = pygame.image.load(path.join(img_dir, "imagem_fundo.png")).convert()
 background_rect = background.get_rect()
+
+try:
+    
+    # Loop principal.
+    running = True
+    while running:
+        
+    
+        
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            
+            # Verifica se foi fechado
+            if event.type == pygame.QUIT:
+                running = False
+    
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+finally:
+    pygame.quit()
+
+
 
 
