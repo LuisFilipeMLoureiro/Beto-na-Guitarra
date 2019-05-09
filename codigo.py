88b3435fc2fc2dee6a757cfdde3ad6f843a3912a
unknown <luisfml@al.insper.edu.br>
2019-05-09 16:32:51 -0300

    deixando o código do zero

--- codigo.py
+++ codigo.py
@@ -5,13 +5,14 @@
 # Importando as bibliotecas necessárias.
 import pygame
 from os import path
-
+import time
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'imagens')
 
 
+
 # Carrega o fundo do jogo
-background = pygame.image.load(path.join(img_dir, '"imagem_de_fundo.jpg"')).convert()
+background = pygame.image.load(path.join(img_dir, '"imagem_fundo.jpg"')).convert()
 background_rect = background.get_rect()
 
 
