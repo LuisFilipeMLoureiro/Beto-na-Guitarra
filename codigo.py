6b8f947808afad62df97b623165ff689e46322be
robertoschno <robertoschno@gmail.com>
2019-05-24 01:51:11 -0300

    hino da urss, ajuste dos sons e eh isso sepa

--- codigo.py
+++ codigo.py
@@ -191,8 +191,8 @@ SpeedyBull=-15
 SpeedxBull=0
 
 #sons do jogo:
-pygame.mixer.music.load(path.join(snd_dir, 'Official Opening Credits Game of Thrones (HBO).wav'))
-pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.load(path.join(snd_dir, 'hino_URSS.ogg'))
+pygame.mixer.music.set_volume(0.25)
 som_tiro1=pygame.mixer.Sound(path.join(snd_dir, 'bang2.ogg'))
 som_tiro2=pygame.mixer.Sound(path.join(snd_dir, 'plaa.ogg'))
 som_zumbi_morrendo1=pygame.mixer.Sound(path.join(snd_dir, 'zumbi_morrendo.ogg'))
@@ -350,7 +350,7 @@ try:
                 som_zumbi_morrendo1.play()
             if sz < 19:
                 som_zumbi_morrendo2.play()
-            else:
+            if sz > 38:
                 som_zumbi_morrendo3.play()
 
             
